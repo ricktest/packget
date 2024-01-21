4df5d0602d75e97cf191910bfaf20fd8a0508dac
huang <newrobert20485@gmail.com>
2024-01-21 14:42:51 +0800

    2

--- src/Test.php
+++ src/Test.php
@@ -2,11 +2,19 @@
 
 namespace Virtualorz\Hellow;
 
+use Illuminate\Support\Facades\DB;
+
 class Test
 {
     public function test()
     {
         echo 'asdasdasdasdasda';
+    }
+
+    public function dbTest()
+    {
+        $data = DB::table('member_info')->get();
+        dd($data);
     }
 }
 ?>
